bfa82c8aa5bc7b27c53d02fd2a7544bbe1583241
David Mehedinti <49506223+DM1224@users.noreply.github.com>
2024-12-10 10:49:12 -0600

    Initial Commit

---
+++ Recipe ViewerTests/Recipe_ViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ViewerTests.swift
+//  Recipe ViewerTests
+//
+//  Created by David Mehedinti on 12/10/24.
+//
+
+import Testing
+@testable import Recipe_Viewer
+
+struct Recipe_ViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
