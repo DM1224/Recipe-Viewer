fb3ee827e78ed1bf1d6adc28de598b36914d1ed7
David Mehedinti <49506223+DM1224@users.noreply.github.com>
2024-12-10 17:55:22 -0600

    Small UI details and cleanup

--- Recipe Viewer/ContentView.swift
+++ Recipe Viewer/ContentView.swift
@@ -12,12 +12,77 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var recipes: [RecipeEntity]
     
+    func recipePage(recipe: RecipeEntity) -> some View {
+        VStack {
+            HStack {
+                if let data = recipe.photoLarge, let uiImage = UIImage(data: data) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                } else {
+                    Image(systemName: "fork.knife.circle.fill") // Placeholder
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .foregroundColor(.gray)
+                }
+            }
+            .onAppear {
+                if recipe.photoLarge == nil {
+                    Task {
+                        await recipe.fetchImage(forSmallImage: false)
+                    }
+                }
+            }
+            Text(recipe.name)
+                .font(.title)
+            Text("Cuisine: \(recipe.cuisine)")
+            Button(action: {
+                if let url = recipe.sourceUrl {
+                    UIApplication.shared.open(url)
+                }
+            }) {
+                HStack {
+                    Image(systemName: "safari")
+                        .font(.headline)
+                    Text("Visit Website")
+                        .fontWeight(.bold)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+            .padding()
+            Button(action: {
+                if let url = recipe.youtubeUrl {
+                    UIApplication.shared.open(url)
+                }
+            }) {
+                HStack {
+                    Image(systemName: "video")
+                        .font(.headline)
+                    Text("Watch YouTube Video")
+                        .fontWeight(.bold)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+            .padding()
+            Spacer()
+        }
+    }
+    
     func recipeList() -> some View {
         ScrollViewReader { proxy in
             List {
                 ForEach(recipes) { recipe in
                     NavigationLink {
-                        Text(recipe.name)
+                        recipePage(recipe: recipe)
                     } label: {
                         HStack {
                             if let data = recipe.photoSmall, let uiImage = UIImage(data: data) {
@@ -26,7 +91,7 @@ struct ContentView: View {
                                     .frame(width: 50, height: 50)
                                     .clipShape(Circle())
                             } else {
-                                Image(systemName: "fork.knife.circle.fill") // Placeholder
+                                Image(systemName: "fork.knife.circle.fill")
                                     .resizable()
                                     .frame(width: 50, height: 50)
                                     .foregroundColor(.gray)
@@ -44,6 +109,7 @@ struct ContentView: View {
                 }
             }
         }
+        .navigationTitle("Recipes")
     }
 
     var body: some View {
